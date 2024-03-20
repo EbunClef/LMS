654063354b1b5b10044972b239d0a2fbdf4ecc69
Young Ayodele <young.ayodele@gmail.com>
2024-03-20 11:35:03 +0100

    Modified Member class

--- member.py
+++ member.py
@@ -1,52 +1,45 @@
-from book import Book
-from library import Library
-
 class Member:
     def __init__(self, member_id, name):
         self.member_id = member_id
         self.name = name
         self.books_borrowed = []
-
-    def borrow_book(self, book_or_title, library):
-        if isinstance(book_or_title, str):
-            # Borrow a book by title
-            book_to_borrow = library.get_book_by_title(book_or_title)
-            if book_to_borrow is None:
-                print(f"Book with title '{book_or_title}' not found in the library.")
-                return False
-        elif isinstance(book_or_title, Book):
-            # Borrow a book by book object
-            book_to_borrow = book_or_title
-        else:
-            print("Invalid argument. Please provide a book object or a book title.")
-            return False
-
-        if book_to_borrow.quantity <= 0:
-            print("No available copies of the book.")
-            return False
         
-        book_to_borrow.quantity -= 1
-        self.books_borrowed.append(book_to_borrow)
-        print("Book borrowed successfully.")
-        return True
-
-    def return_book(self, book):
-        if book in self.books_borrowed:
-            book.quantity += 1
-            self.books_borrowed.remove(book)
-            print("Book returned successfully.")
-            return True
+    def borrow_book(self, library, book_title):
+        # Find the book in the library by title
+        book_to_borrow = None
+        for book in library.library:
+            if book.title == book_title:
+                book_to_borrow = book
+                break
+        
+        if book_to_borrow is None:
+            return "Book with title '{}' not found in the library.".format(book_title), None
+        
+        # Check if the book is available
+        if book_to_borrow.quantity > 0:
+            # Reduce the quantity of available copies
+            book_to_borrow.quantity -= 1
+            # Add the book to the member's list of borrowed books
+            self.books_borrowed.append(book_to_borrow)
+            return "Book '{}' successfully borrowed by {}. Quantity left: {}.".format(book_title, self.name, book_to_borrow.quantity), book_to_borrow.quantity
         else:
-            print("Book not borrowed by this member.")
-            return False
-
+            return "Book '{}' is not available for borrowing.".format(book_title), None
+    
+        
+    def return_book(self, book):
+        # Allows a member to return a book
+        if book in self.books_borrowed:
+            self.books_borrowed.remove(book)
+        
     def get_borrowed_books(self):
+        # Returns a list of dictionaries representing the books borrowed by the member
         borrowed_books_info = []
         for book in self.books_borrowed:
             borrowed_books_info.append(book.to_dict())
         return borrowed_books_info
-
+    
     def to_dict(self):
+        # Returns a dictionary representation of the member
         return {
             'member_id': self.member_id,
             'name': self.name,
